6abd1cc9cc012d5d82c2ed7f172211065419ffc6
ThiagoLima1996 <thiago.lima.leite@gmail.com>
2023-06-04 12:47:06 -0300

    Inicio das modifficações no MovieCarrousel, ja esta exibindo o banner dos filmes e a nota deles

--- src/components/MovieCarousel.js
+++ src/components/MovieCarousel.js
@@ -4,19 +4,24 @@ function MovieCarousel({ title, items }) {
 
     return (
         <div>
-            <h2>{title}</h2>
+            <h2 className=' text-3xl font-semibold dark:text-white'>{title}</h2>
 
-            <div>
+            <div className='flex gap-2'>
 
                 {items.results.length > 0 && items.results.map((item, key) => {
                     console.log(item);
                     return (
-                        <div>
+                        <div className=' relative'>
                             <img
                                 src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
-                                alt={item.original_name
+                                alt={item.original_title
                                 }
+                                className=' cursor-pointer'
+                                key={key}
                             />
+                            <p className=' absolute bg-yellow-500 -bottom-1 -right-1 rounded-full w-6 h-6 flex items-center justify-center text-xs'>
+                                {item.vote_average.toFixed(1)}
+                            </p>
                         </div>
 
 
